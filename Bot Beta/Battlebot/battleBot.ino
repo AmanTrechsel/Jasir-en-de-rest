271240a855080e2d91387b5b377529dc317e577e
JesperSchuurman <jesper.schuurman@student.nhlstenden.com>
2023-03-28 11:20:17 +0200

    added the finnish sequence
    also re added the method that finds the finnish/exit since it was accidentally removed

--- Bot Beta/Battlebot/battleBot.ino
+++ Bot Beta/Battlebot/battleBot.ino
@@ -126,6 +126,11 @@ void setup() {
       goLeft(); //after 0.4 seconds go left
       delay(950); // turn left for 0.95 seconden
       brake(); // stop
+      closeGripper();
+      delay(200);
+      goLeft();
+      delay(900);
+      brake();
       delay(50);
       goForward(); // go forward in to the maze
       delay(450);
@@ -157,6 +162,36 @@ void solveMaze()
   delay(500);
   echoSensor();
   if (distance < minSafeDistance)
+  if (distance >= 50)
+  {
+    frontScan();
+    delay(500);
+    echoSensor();
+    if (distance >= 50);
+    {
+      leftScan();
+      delay(500);
+      echoSensor();
+      if (distance >= 50);
+      {
+        goToFinnish();
+        uint16_t position = qtr.readLineBlack(sensorValues);   
+        //stop when all sensors detect black    
+        if((sensorValues[0] > 700) && (sensorValues[1] > 700) && (sensorValues[2] > 700) && (sensorValues[3] > 700) && (sensorValues[4] > 700) && (sensorValues[5] > 700) && (sensorValues[6] > 700) && (sensorValues[7] > 700))   
+        {     
+          brake();    
+          delay(50);     
+          openGripper();  
+          brake();
+          backUp();
+          delay(500);
+          brake();
+          finishMusic();
+        }
+      }
+    }
+  }
+  else if (distance < minSafeDistance)
   {
     lightsBad();
     delay(200);
@@ -215,6 +250,15 @@ void solveMaze()
     movementValue = 30;
     moveForward();
   }
+}
+
+void goToFinnish()
+{
+  counter1 = 0;
+  counter2 = 0;
+  brake();
+  movementValue = 10;
+  moveForward();
 }
 
 
@@ -289,6 +333,12 @@ void goLeft() // move to the left
   analogWrite(motorA2, 0); //left forwards
   analogWrite(motorB1, 0); //right backwards
   analogWrite(motorB2, 225); //right forwards
+}
+
+void backUp() //the bot moves backwards
+{
+  rightBackward();
+  leftBackward();
 }
 
 void moveForward() // move forward with pulses
